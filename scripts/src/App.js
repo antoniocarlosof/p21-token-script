53dd2bb2f80f3953b93e6515d06df1da94d6fd20
Antônio Carlos <antonio@p21.local>
2022-09-23 18:14:41 -0300

    Web3 connection stablished. Constructor and state set.

--- scripts/src/App.js
+++ scripts/src/App.js
@@ -1,8 +1,33 @@
+import Web3 from 'web3';
 import { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    this.loadBlockchainData()
+  }
+
+  async loadBlockchainData() {
+    const web3Connection = new Web3(Web3.givenProvider || "http://localhost:8545")
+
+    const accounts = await web3Connection.eth.getAccounts()
+    this.setState({ account: accounts[0]})
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = { 
+      account: '',
+      loading: true
+    }
+
+    this.allowSystem = this.allowSystem.bind(this)
+    this.buy = this.buy.bind(this)
+    this.offer = this.offer.bind(this)
+  }
+  
   return (
+
   );
 }
 
