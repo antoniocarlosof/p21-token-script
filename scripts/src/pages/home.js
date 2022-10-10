a72944d9836d3f9660dd350cb83007c628b03246
Antônio Carlos <antonio.carlos.o.ferreira@gmail.com>
2022-10-10 15:59:18 -0300

    Home page set up with 'Offers' button.

--- scripts/src/pages/home.js
+++ scripts/src/pages/home.js
@@ -1,5 +1,21 @@
 import { Component } from 'react';
+import { Link } from "react-router-dom";
 
-class Home extends Component{}
+class Home extends Component{
+    render(){
+        return(
+            <div className='container-fluid'>
+                <div className='row justify-content-center'>
+                    <div className='col-4'>
+                        <Link
+                            type='button'
+                            className='btn btn-success'
+                            to="/offers">Offers</Link>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
 
 export default Home;
