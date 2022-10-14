cde4b13c93595798ca6b34ddebb540b5dea9050a
Antônio Carlos <antonio.carlos.o.ferreira@gmail.com>
2022-10-14 11:54:10 -0300

    Sell class structure mounted.

---
+++ scripts/src/pages/sell.js
@@ -0,0 +1,9 @@
+import Web3 from 'web3';
+import { Component } from 'react';
+import { OFFER_LIST_ABI, OFFER_LIST_ADDRESS } from './config.js';
+
+class Sell extends Component{
+
+}
+
+export default Sell;
